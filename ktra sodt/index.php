6c5a4b58bfa6e9e970ef4e2e7feeb89277edbf8d
Thanh23198 <nguyendinhthanh2311998@gmail.com>
2020-11-24 10:43:20 +0700

    add file

--- ktra sodt/index.php
+++ ktra sodt/index.php
@@ -1,11 +1,11 @@
 <?php
-function isPhoneNumber($str){
-    $regexp = '/^[0-9]{2}+(\-[0-9]{10}+)$/';
-    if(preg_match($regexp,$str)){
-        echo "PhoneNumber is allowed";
-    }
-    else{
-        echo "PhoneNumber is not allowed";
+function checkPhone($str) {
+    $regex = '/^\([0-9]{2}\)+-\([0]{1}[0-9]{9}\)$/';
+    if (preg_match($regex, $str)) {
+        echo $str." So dien thoai hop le". '<br/>';
+    } else {
+        echo $str." So dien thoai khong hop le". '<br/>';
     }
 }
-isPhoneNumber('(84)-(0978489648)');
+checkPhone('(84)-(0978489648)');
+checkPhone(' (a8)-(22222222)');
